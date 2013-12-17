0c98372f6d79cb5745a98b57310c039207dbfd56
padutumich <padot@umich.edu>
2013-12-17 12:22:05 -0500

    Added some stuff to web.config to allow json and cors; added some schedule json files and URLs.

--- webapp/app/schedule/scheduleService.js
+++ webapp/app/schedule/scheduleService.js
@@ -15,7 +15,7 @@ function ScheduleService($http, $q, $scope, $rootScope){
 
 	this.getInfo = function(str_date){
 		var info = {
-			jsonURL 	: 'json/passthrough?feedurl='+escape('https://iu.edu/~iumobile/kme3beta/conference/json/processed/schedule/'+str_date+'.json'),
+		    jsonURL: 'http://ifdtc.azurewebsites.net/data/schedule/' + str_date + '.json',
 			description	: 'Schedule of all the sesssion. Have fun!'
 		}
 		return info;
