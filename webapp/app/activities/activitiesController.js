2dda57a421c5fc182e8aeae0a2004b50a8712d1c
OpenShift guest <rfenton@umich.edu>
2014-02-11 14:04:04 -0500

    Removed need to click refresh button.  Now gets data after form has been validated and submitted.

--- webapp/app/activities/activitiesController.js
+++ webapp/app/activities/activitiesController.js
@@ -48,6 +48,30 @@ function ActivitiesCtrl($scope,$rootScope,$route,$http,$window,activitiesService
     /*
         DISPLAY Code
     */
+    
+    $scope.evaluationFormSubmission = function(formID, loaderID, completeID, buttonID){
+    $("#"+formID).submit();
+    $("#"+loaderID).show();
+    setTimeout( 'killEvaluationFormSubmissionLoader("'+loaderID+'")', 900);
+    setTimeout( 'showEvaluationFormSubmissionComplete("'+completeID+'")', 910);
+    setTimeout( 'disableEvaluationFormSubmissionButton("'+buttonID+'")', 10);
+    }
+
+    $scope.disableEvaluationFormSubmissionButton = function(buttonID){
+    $("#"+buttonID).addClass("disabled");
+    }
+    $scope.killEvaluationFormSubmissionLoader = function(loaderID){
+    $("#"+loaderID).hide();   
+    }
+
+    $scope.showEvaluationFormSubmissionComplete = function(completeID){
+    $("#"+completeID).show();
+
+    // hack to hide forms on home/dashboard screen
+    $( "#HomepageEvalForm01" ).fadeOut( 1600 );
+    $( "#HomepageEvalForm02" ).fadeOut( 1600 );
+    }
+
 
 
     // INIT
